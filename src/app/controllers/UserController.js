7fc3f1353b575c2ce704441b035be9dbe2474916
Gustavo Morini <gustavommh@gmail.com>
2020-01-30 13:45:15 -0300

    validate req.body create, update, session

--- src/app/controllers/UserController.js
+++ src/app/controllers/UserController.js
@@ -1,7 +1,24 @@
+// a importação do Yup tem que ser feita com *
+// pois ele não tem nenhum export default
+import * as Yup from 'yup';
 import User from '../models/User';
 
 class UserController {
   async store(req, res) {
+    const schema = Yup.object().shape({
+      name: Yup.string().required(),
+      email: Yup.string()
+        .email()
+        .required(),
+      password: Yup.string()
+        .required()
+        .min(6),
+    });
+
+    if (!(await schema.isValid(req.body))) {
+      return res.status(400).json({ error: 'Validation failed' });
+    }
+
     const userExists = await User.findOne({
       where: {
         email: req.body.email,
@@ -22,6 +39,28 @@ class UserController {
   }
 
   async update(req, res) {
+    // when() é uma condição dentro da verificação
+    // no caso se oldPassword existir password tbm é obrigatório
+    // oneOf compara os valores para ver se são iguais
+    // Yup.ref() cria uma referência a qual campo deve ser feita a comparação
+    const schema = Yup.object().shape({
+      name: Yup.string(),
+      email: Yup.string().email(),
+      oldPassword: Yup.string().min(6),
+      password: Yup.string()
+        .min(6)
+        .when('oldPassword', (oldPassword, field) =>
+          oldPassword ? field.required() : field
+        ),
+      confirmPassword: Yup.string().when('password', (password, field) =>
+        password ? field.required().oneOf([Yup.ref('password')]) : field
+      ),
+    });
+
+    if (!(await schema.isValid(req.body))) {
+      return res.status(400).json({ error: 'Validation failed' });
+    }
+
     const { email, oldPassword } = req.body;
     const user = await User.findByPk(req.userId);
 
