e48d6736d0b5da41709a83177d5269cbe1d764e5
Gustavo Morini <gustavommh@gmail.com>
2020-02-02 13:44:13 -0300

    op between start/end day

--- src/app/controllers/ScheduleController.js
+++ src/app/controllers/ScheduleController.js
@@ -1,3 +1,5 @@
+import { startOfDay, endOfDay, parseISO } from 'date-fns';
+import { Op } from 'sequelize';
 import Appointment from '../models/Appointment';
 import User from '../models/User';
 
@@ -13,7 +15,21 @@ class SqueduleController {
     if (!checkUserProvider)
       res.status(401).json({ error: 'user is not a provider' });
 
-    return res.json({});
+    const { date } = req.query;
+
+    const parseDate = parseISO(date);
+
+    const appointments = await Appointment.findAll({
+      where: {
+        provider_id: req.userId,
+        canceled_at: null,
+        date: {
+          [Op.between]: [startOfDay(parseDate), endOfDay(parseDate)],
+        },
+      },
+    });
+
+    return res.json(appointments);
   }
 }
 
