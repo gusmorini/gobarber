2449b2cfc3f322bc87c604e365be8af139e18fee
Gustavo Morini <gustavommh@gmail.com>
2020-02-02 11:15:31 -0300

    appointment, migration + model

---
+++ src/app/models/Appointment.js
@@ -0,0 +1,25 @@
+import Sequelize, { Model } from 'sequelize';
+
+class Appointment extends Model {
+  static init(sequelize) {
+    super.init(
+      {
+        date: Sequelize.DATE,
+        canceled_at: Sequelize.DATE,
+      },
+      {
+        sequelize,
+      }
+    );
+    return this;
+  }
+
+  static associate(models) {
+    // quando existe mais de um relacionamento da tabela
+    // é obrigatório usar o apelido "as:" para identificar cada um deles
+    this.belongsTo(models.User, { foreignKey: 'user_id', as: 'user' });
+    this.belongsTo(models.User, { foreignKey: 'provider_id', as: 'provider' });
+  }
+}
+
+export default Appointment;
