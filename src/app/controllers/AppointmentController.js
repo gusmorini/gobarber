c3243c51039f39a48dad1435d154da5ba10e7897
Gustavo Morini <gustavommh@gmail.com>
2020-02-02 13:06:42 -0300

    pagination

--- src/app/controllers/AppointmentController.js
+++ src/app/controllers/AppointmentController.js
@@ -6,6 +6,8 @@ import File from '../models/File';
 
 class AppointmentController {
   async index(req, res) {
+    const { page = 1 } = req.query;
+    const limit = 10;
     const appointments = await Appointment.findAll({
       where: {
         user_id: req.userId,
@@ -13,6 +15,8 @@ class AppointmentController {
       },
       order: ['date'],
       attributes: ['id', 'date'],
+      limit,
+      offset: (page - 1) * limit,
       include: [
         {
           model: User,
