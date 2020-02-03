4adc5448e1d2b44c69ca32fcf742952017852b4f
Gustavo Morini <gustavommh@gmail.com>
2020-02-03 15:16:03 -0300

    cancel appointment

--- src/routes.js
+++ src/routes.js
@@ -21,12 +21,18 @@ routes.post('/sessions', SessionController.store);
 routes.use(authMiddleware);
 // authenticated routes
 routes.put('/users', UserController.update);
+
 routes.get('/providers', ProviderController.index);
+
 routes.post('/appointments', AppointmentController.store);
 routes.get('/appointments', AppointmentController.index);
+routes.delete('/appointments/:id', AppointmentController.delete);
+
 routes.get('/schedule', SqueduleController.index);
+
 routes.get('/notifications', NotificationController.index);
 routes.put('/notifications/:id', NotificationController.update);
+
 routes.post('/files', upload.single('file'), FileController.store);
 
 export default routes;
