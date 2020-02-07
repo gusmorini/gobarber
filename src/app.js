91fce91052d91c643b9a6b2e775cd6bbe56f94ab
Gustavo Morini <gustavommh@gmail.com>
2020-02-07 13:26:00 -0300

    error handling, Sentry, Youch, express-async-erros

--- src/app.js
+++ src/app.js
@@ -1,17 +1,27 @@
 import express from 'express';
+import 'express-async-errors';
+
 import path from 'path';
+import Youch from 'youch';
+
+import * as Sentry from '@sentry/node';
 import routes from './routes';
+import sentrycfg from './config/sentry';
 
 import './database';
 
 class App {
   constructor() {
     this.server = express();
+    Sentry.init(sentrycfg);
+
     this.middlewares();
     this.routes();
+    this.exceptionHandle();
   }
 
   middlewares() {
+    this.server.use(Sentry.Handlers.requestHandler());
     this.server.use(express.json());
     this.server.use(
       '/files',
@@ -21,6 +31,14 @@ class App {
 
   routes() {
     this.server.use(routes);
+    this.server.use(Sentry.Handlers.errorHandler());
+  }
+
+  exceptionHandle() {
+    this.server.use(async (err, req, res, next) => {
+      const errors = await new Youch(err, req).toJSON();
+      return res.status(500).json(errors);
+    });
   }
 }
 
---
+++ src/config/sentry.js
@@ -0,0 +1,3 @@
+export default {
+  dsn: 'https://7cd4198365fc4e2fa68dfdf6f4df3a41@sentry.io/2327097',
+};
